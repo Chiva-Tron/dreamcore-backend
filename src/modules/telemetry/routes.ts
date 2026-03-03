25f52e178ffe46ba95856b66298caf7b056c2cb0
Esteban Moreyra <moreyraesteban@gmail.com>
2026-03-03 10:28:51 -0300

    Mejoras

--- src/modules/telemetry/routes.ts
+++ src/modules/telemetry/routes.ts
@@ -6,15 +6,20 @@ import { ingestTelemetryBatch } from "./service";
 
 export const telemetryRouter = Router();
 
+const submitTelemetryBatch = asyncHandler(async (req, res) => {
+  const data = await ingestTelemetryBatch({
+    auth: res.locals.auth,
+    payload: req.body
+  });
+
+  return sendOk(res, 202, data);
+});
+
 telemetryRouter.post(
   "/events/batch",
   requireAuth,
-  asyncHandler(async (req, res) => {
-    const data = await ingestTelemetryBatch({
-      auth: res.locals.auth,
-      payload: req.body
-    });
-
-    return sendOk(res, 202, data);
-  })
+  submitTelemetryBatch
 );
+
+telemetryRouter.post("/submit_run", requireAuth, submitTelemetryBatch);
+telemetryRouter.post("/submit-run", requireAuth, submitTelemetryBatch);
